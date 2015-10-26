7b3e6a25e55466f52f54f819bc33b8568d899150
unknown <sbaird02@CSPC121.dcs.bbk.ac.uk>
2015-10-26 14:07:20 +0000

    Exercise 1,2

---
+++ Exercise1.java
@@ -0,0 +1,44 @@
+import java.util.Random;
+
+public class Exercise1 {
+
+
+	public static void main(String[] args) {
+		
+//		new Exercise1().exercise1Runner_1();
+		new Exercise1().exercise1Runner_1_Random();
+	
+	}
+	
+	private void exercise1Runner_1() {
+	
+		IntegerTreeNode root = new IntegerTreeNode(55);
+		root.add(13);
+		root.add(66);
+		int lookFor = 13;
+		System.out.println("Tree " + (root.contains(lookFor) ? "CONTAINS " : "DOES NOT contain ") + lookFor );
+		System.out.println("Search for " +  lookFor + " took " + root.searchFor(lookFor) + " iterations" );
+		System.out.println(root);
+		
+	}
+	
+	private void exercise1Runner_1_Random() {
+	
+		long  observationCount = 10000;
+		IntegerTreeNode root = new IntegerTreeNode();
+		Random r = new Random(observationCount);
+		int lookFor = 0;
+					
+		for (long l = 0; l < observationCount; l++) {
+			
+			lookFor = r.nextInt();
+			root.add(lookFor);
+		}
+		System.out.println("Tree " + (root.contains(lookFor) ? "CONTAINS " : "DOES NOT contain ") + lookFor );
+		System.out.println("Search for " +  lookFor + " took " + root.searchFor(lookFor) + " iterations" );
+		
+		System.out.println("Tree " + (root.contains(lookFor-1) ? "CONTAINS " : "DOES NOT contain ") + (lookFor-1) );
+		System.out.println("Search for " +  (lookFor-1) + " took " + root.searchFor(lookFor-1) + " iterations" );		
+		System.out.println(root);
+	}
+}
--- IntegerTreeNode.java
+++ IntegerTreeNode.java
@@ -1,12 +1,24 @@
 public class IntegerTreeNode {
 
-    private final int value;
+    private int value;
     private IntegerTreeNode leftNode;
     private IntegerTreeNode rightNode;
+	private StringBuilder sb;
+	//
+	private final String EMPTY_BRACKETS = "%s[]";
+	private final String BRACKETS = "%s[%s]";
+	
 
     public IntegerTreeNode(int value) {
-
+		
+		this();
         this.value = value;
+    }
+	
+	public IntegerTreeNode() {
+
+		leftNode = null;
+		rightNode = null;
     }
 
     public void add(int newNumber) {
@@ -44,4 +56,50 @@ public class IntegerTreeNode {
             }
         }
     }
+	
+	public int searchFor(int n) {
+        
+        if (n == this.value) {
+            return 1;
+        } else if (n > this.value) {
+            if (rightNode == null) {
+                return 0;
+            } else {
+                return rightNode.searchFor(n) + 1;
+            }
+        } else {
+            if (leftNode == null) {
+                return 0;
+            } else {
+                return leftNode.searchFor(n) + 1;
+            }
+        }
+    }
+	
+	public int getMax(){
+		
+		if ( rightNode == null) {
+			return value;
+		} else {
+			return rightNode.getMax();
+		}		
+	}
+	
+	public int getMin() {
+		
+		if ( leftNode == null) {
+			return value;
+		} else {
+			return leftNode.getMin();
+		}	
+	}
+	
+	public String toString() {
+	
+		sb = new StringBuilder();
+		sb.append(value==0 ? String.format(BRACKETS,"","") : String.format(BRACKETS,"",value))
+			.append(leftNode == null ? String.format(BRACKETS,"L","") : String.format(BRACKETS,"L",leftNode) )
+			.append(rightNode == null ? String.format(BRACKETS,"R","") : String.format(BRACKETS,"R",rightNode) );
+		return sb.toString();
+	}
 }
